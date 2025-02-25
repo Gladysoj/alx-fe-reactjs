c447b3a74b2af245b438a22ae606bb6481bce1f5
Gladys <114776170+Gladysoj@users.noreply.github.com>
2025-02-25 14:55:38 +0400

    Props Drilling

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,20 +1,14 @@
-import WelcomeMessage from './components/WelcomeMessage';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
-import Counter from './components/Counter';
+import { UserContext } from './components/UserContext';
+import ProfilePage from './ProfilePage';
 
 function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com", age: 25, bio: "Loves hiking and photography" };
+
   return (
-    <div>
-      <WelcomeMessage />
-      <Header />
-      <MainContent />
-      <Footer />
-      <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
-    </div>
-  ); 
+    <UserContext.Provider value={userData}>
+      <ProfilePage />;
+    </UserContext.Provider>
+  )
 }
 
-export default App
+export default App;
