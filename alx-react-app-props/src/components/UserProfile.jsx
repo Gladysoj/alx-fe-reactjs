cb7d7e7461638a3345ac79bba6fe2c287718e00d
Gladys <114776170+Gladysoj@users.noreply.github.com>
2025-02-25 14:57:47 +0400

    Props Drilling

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,10 +1,16 @@
-const UserProfile = (props) => {
+import { useContext } from "react";
+import UserContext from "./UserContext";
+
+const UserProfile = (userData) => {
+
+    const userData = useContext(UserContext);
+
     return (
-      <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-   <h2 style={{ color: 'blue' }}>{props.name}</h2>
-   <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
-   <p>Bio: {props.bio}</p>
- </div>
+      <div>
+        <h2>{userData.name}</h2>
+        <p>Age: {userData.age}</p>
+        <p>Bio: {userData.bio}</p>
+      </div>
     );
 };
 
