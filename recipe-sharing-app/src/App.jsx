b5185c4b77c2ccba2057c0f0d28055866db9a42f
Gladys <114776170+Gladysoj@users.noreply.github.com>
2025-02-26 12:53:22 +0400

    Recipe-Sharing-App

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import  RecipeList from './components/RecipeList'
-import AddRecipeForm from './components/AddRecipeForm'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import  RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
 
 function App() {
   const [count, setCount] = useState(0)
