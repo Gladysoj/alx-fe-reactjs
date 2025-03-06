e0ccd12f76bc94e370a94baddffa759880ce596c
Gladys <114776170+Gladysoj@users.noreply.github.com>
2025-03-06 16:50:46 +0400

    New Commit File

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,5 +1,6 @@
+
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'; // Explicit imports
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/Home';
 import Profile from './components/Profile';
 import BlogPost from './components/BlogPost';
@@ -10,16 +11,13 @@ function App() {
   const [isAuthenticated, setIsAuthenticated] = React.useState(false);
 
   return (
-    // BrowserRouter component wrapping the entire app
     <BrowserRouter>
       <div className="App">
         <Navbar 
           isAuthenticated={isAuthenticated}
           setIsAuthenticated={setIsAuthenticated}
         />
-        {/* Routes component containing all Route definitions */}
         <Routes>
-          {/* Individual Route components */}
           <Route path="/" element={<Home />} />
           <Route
             path="/profile"
@@ -29,7 +27,8 @@ function App() {
               </ProtectedRoute>
             }
           />
-          <Route path="/blog/:postId" element={<BlogPost />} />
+          
+          <Route path="/blog/:id" element={<BlogPost />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
@@ -38,3 +37,4 @@ function App() {
 }
 
 export default App;
+
