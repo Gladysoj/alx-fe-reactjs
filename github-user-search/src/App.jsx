2c0542f11c38de99977c7097e71144eeed587734
Gladys <114776170+Gladysoj@users.noreply.github.com>
2025-03-25 15:41:30 +0400

    New Commit File for APIs

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,11 +1,10 @@
 import Search from './components/Search';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <h1>GitHub User Search</h1>
+    <div>
+      <header className="bg-blue-600 text-white p-4 text-center">
+        <h1 className="text-2xl font-bold">GitHub User Search</h1>
       </header>
       <main>
         <Search />
