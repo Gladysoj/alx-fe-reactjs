3095adb7722328247e0705a038023f39d3acba77
gladysuchola <ucholagladys@gmail.com>
2025-03-25 13:33:40 +0400

    APIs 10th commit message

---
+++ github-user-search/src/services/api.js
@@ -0,0 +1,12 @@
+// src/services/api.js
+import axios from 'axios';
+
+export const githubApi = axios.create({
+  baseURL: 'https://api.github.com',
+  headers: {
+    'Accept': 'application/vnd.github.v3+json',
+    ...(import.meta.env.VITE_GITHUB_API_KEY && {
+      'Authorization': `token ${import.meta.env.VITE_GITHUB_API_KEY}`
+    })
+  }
+});
